91642e5c13617d675525862d10e1e7d694a50d82
Bret Comnes <bcomnes@gmail.com>
2015-05-10 12:02:44 -0700

    Working menu bar

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var app = require('app')  // Module to control application life.
+var BrowserWindow = require('browser-window')  // Module to create native browser window.
+var Tray = require('tray')
+var Menu = require('menu')
 
 // Report crashes to our server.
 require('crash-reporter').start()
@@ -8,15 +10,26 @@ require('crash-reporter').start()
 // be closed automatically when the javascript object is GCed.
 var mainWindow = null
 
+var appIcon = null
+
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  if (process.platform != 'darwin')
-    app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function () {
+  if (app.dock) app.dock.hide()
+  appIcon = new Tray('./193Template.png')
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio'}
+  ])
+  appIcon.setToolTip('This is my application.')
+  appIcon.setContextMenu(contextMenu)
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
