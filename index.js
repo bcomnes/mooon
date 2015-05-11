2f1371ea8f01d3869c9cf6abb53eb8755d5f9841
Bret Comnes <bcomnes@gmail.com>
2015-05-10 20:03:14 -0700

    Basic setting of base icon

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
 var app = require('app')  // Module to control application life.
-var BrowserWindow = require('browser-window')  // Module to create native browser window.
 var Tray = require('tray')
 var Menu = require('menu')
+var moonmoji = require('moonmoji')
 
 // Report crashes to our server.
 require('crash-reporter').start()
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
-var mainWindow = null
+var phases = {
+  'New Moon': '189',
+  'Waxing Crescent': '190',
+  'First Quarter': '191',
+  'Waxing Gibbous': '192',
+  'Full Moon': '193',
+  'Waning Gibbous': '194',
+  'Last Quarter': '195',
+  'Waning Crescent': '196'
+}
 
 var appIcon = null
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function () {
   if (app.dock) app.dock.hide()
-  var phase = suncalc.getMoonIllumination(date).phase
-  appIcon = new Tray('./Icon.png')
+  var phase = moonmoji().name
+  appIcon = new Tray('./icons/' + phases[phase] + '.png')
   var contextMenu = Menu.buildFromTemplate([
-    { label: phase, type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio'}
+    {
+      label: 'Quit',
+      accelerator: 'Command+Q',
+      click: function () { app.quit() }
+    }
   ])
-  appIcon.setToolTip('This is my application.')
+  appIcon.setToolTip('Mooon')
   appIcon.setContextMenu(contextMenu)
-  // Create the browser window.
-  // mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  // mainWindow.loadUrl('file://' + __dirname + '/index.html')
-
-// Emitted when the window is closed.
-// mainWindow.on('closed', function () {
-// Dereference the window object, usually you would store windows
-// in an array if your app supports multi windows, this is the time
-// when you should delete the corresponding element.
-// mainWindow = null
-// })
 })
