8fd50575ad303cd7782fb1c6b74858be90008beb
Bret Comnes <bcomnes@gmail.com>
2015-05-15 08:40:14 -0700

    Basic packaging test

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var app = require('app')  // Module to control application life.
+var BrowserWindow = require('browser-window')
 var Tray = require('tray')
 var Menu = require('menu')
 var moonmoji = require('moonmoji')
+var schedule = require('node-schedule')
 
 // Report crashes to our server.
 require('crash-reporter').start()
@@ -17,14 +19,19 @@ var phases = {
   'Waning Crescent': '196'
 }
 
+var mainWindow = null
 var appIcon = null
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
+
+app.on('error', function (err) { console.error(err)})
+
 app.on('ready', function () {
   if (app.dock) app.dock.hide()
+  // mainWindow = new BrowserWindow({width: 800, height: 600})
   var phase = moonmoji().name
-  appIcon = new Tray('./icons/' + phases[phase] + '.png')
+  appIcon = new Tray(__dirname + '/icons/' + phases[phase] + '.png')
   var contextMenu = Menu.buildFromTemplate([
     {
       label: 'Quit',
@@ -34,4 +41,10 @@ app.on('ready', function () {
   ])
   appIcon.setToolTip('Mooon')
   appIcon.setContextMenu(contextMenu)
+  schedule.scheduleJob({minute: 0}, function () {
+    phase = moonmoji().name
+    console.log(phase)
+    appIcon.setImage(__dirname + '/icons/' + phases[phase] + '.png')
+  })
+// mainWindow.loadUrl('file://' + __dirname + '/index.html')
 })
