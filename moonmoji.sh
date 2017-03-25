695e6a0f642a67c33a8c0df668346871f70cfb77
Bret Comnes <bcomnes@gmail.com>
2017-03-25 13:06:54 -0700

    track bit-bar

---
+++ moonmoji.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# <bitbar.title>Moonmoji</bitbar.title>
+# <bitbar.version>v1.0.0</bitbar.version>
+# <bitbar.author>Bret Comnes</bitbar.author>
+# <bitbar.author.github>bcomnes</bitbar.author.github>
+# <bitbar.desc>Displays current moon phase</bitbar.desc>
+
+PATH=/usr/local/bin:$PATH
+
+moonmoji
